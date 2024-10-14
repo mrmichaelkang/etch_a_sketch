8039adbd7964fc16dc34af066c21b0e8c900d8b7
Michael Kang <mrmichaelkang@gmail.com>
2024-10-13 20:45:12 -0700

    Added reset grid functionality

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const body = document.querySelector("body");
 const container = document.createElement("div");
 const mainContainer = document.createElement("div");
 const infoContainer = document.createElement("div");
+const controlContainer = document.createElement("div");
+const resetButton = document.createElement("button");
+//const gridButton = document.createElement("button");
 const text = document.createElement("p");
 const header = document.createElement("h1");
 const infoTitle = document.createElement("h2");
@@ -10,13 +13,32 @@ let width = 500;
 let height = 500;
 body.style.backgroundColor = "#F3F7EC";
 
+resetButton.textContent = "Reset Grid";
+resetButton.style.border = "none";
+resetButton.style.padding = "15px";
+resetButton.style.borderRadius = "5px";
+resetButton.style.backgroundColor = "#E88D67";
+resetButton.style.color = "#F3F7EC";
+//gridButton.textContent = "New Grid";
+
+resetButton.addEventListener("click", (e) => {
+  removeGrid();
+  generateGrid();
+});
+
+controlContainer.style.display = "flex";
+controlContainer.style.justifyContent = "center";
+
+controlContainer.appendChild(resetButton);
+//controlContainer.appendChild(gridButton);
+
 mainContainer.style.display = "flex";
 mainContainer.style.justifyContent = "space-around";
-mainContainer.style.padding = "50px 350px";
+mainContainer.style.padding = "50px 250px";
 
 infoContainer.style.border = "1px solid black";
-infoContainer.style.width = width;
-infoContainer.style.height = height;
+infoContainer.style.width = "400px";
+infoContainer.style.height = `${height}px`;
 infoContainer.style.textAlign = "center";
 infoContainer.style.padding = "20px";
 infoContainer.style.borderRadius = "15px";
@@ -34,40 +56,56 @@ header.textContent = "Etch-a-Sketch";
 
 container.classList.add("container");
 
-let gridSize = Number(prompt("Enter size of grid (1 - 100):"));
+function removeGrid() {
+  const rows = document.querySelectorAll(".row");
 
-if (gridSize < 1 || gridSize > 100) {
-  do {
-    gridSize = Number(prompt("Enter size of grid (1 - 100):"));
-  } while (gridSize < 1 || gridSize > 100);
+  rows.forEach((row) => {
+    for (let i = 0; i < rows.length; i++) {
+      row.remove();
+    }
+  });
 }
 
-//let gridSize = 16;
-for (let i = 0; i < gridSize; i++) {
-  let row = document.createElement("div");
-  row.style.display = "flex";
+function generateGrid() {
+  let gridSize = Number(prompt("Enter size of grid (1 - 100):"));
 
-  //row.style.float = "right";
-  for (let j = 0; j < gridSize; j++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.style.width = `${width / gridSize}px`;
-    square.style.height = `${height / gridSize}px`;
-    square.style.border = "1px solid black";
-    square.style.borderRadius = "15px";
-    square.style.margin = "1px";
-
-    square.addEventListener("mouseenter", (e) => {
-      e.target.style.backgroundColor = "#E88D67";
-    });
-
-    row.appendChild(square);
+  if (gridSize < 1 || gridSize > 100) {
+    do {
+      gridSize = Number(prompt("Enter size of grid (1 - 100):"));
+    } while (gridSize < 1 || gridSize > 100);
   }
 
-  container.appendChild(row);
+  //let gridSize = 16;
+  for (let i = 0; i < gridSize; i++) {
+    let row = document.createElement("div");
+    row.style.display = "flex";
+    row.classList.add("row");
+
+    //row.style.float = "right";
+    for (let j = 0; j < gridSize; j++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.style.width = `${width / gridSize}px`;
+      square.style.height = `${height / gridSize}px`;
+      square.style.border = "1px solid black";
+      square.style.borderRadius = "15px";
+      square.style.margin = "1px";
+
+      square.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "#E88D67";
+      });
+
+      row.appendChild(square);
+    }
+
+    container.appendChild(row);
+  }
+
+  body.appendChild(header);
+  body.appendChild(controlContainer);
+  mainContainer.appendChild(infoContainer);
+  mainContainer.appendChild(container);
+  body.appendChild(mainContainer);
 }
 
-body.appendChild(header);
-mainContainer.appendChild(infoContainer);
-mainContainer.appendChild(container);
-body.appendChild(mainContainer);
+generateGrid();
